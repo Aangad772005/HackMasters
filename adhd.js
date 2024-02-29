c6c8e3fbaa984d8ef88ab770e035ae023bcc4a12
Aangad <79370124+Aangad772005@users.noreply.github.com>
2024-03-01 00:47:08 +0530

    Add files via upload

---
+++ adhd.js
@@ -0,0 +1,16 @@
+function calculateADHDScore() {
+    const scores = [];
+    for (let i = 1; i <= 10; i++) {
+        const questionId = `question${i}`;
+        const selectedValue = document.getElementById(questionId).value;
+        scores.push(parseInt(selectedValue));
+    }
+
+    const totalScore = scores.reduce((sum, score) => sum + score, 0);
+
+    if (totalScore >= 20) {
+        alert("You might be experiencing ADHD and need to start taking consultancy as soon as possible.");
+    } else {
+        alert("You are safe. If you are unsure, you can take the test again.");
+    }
+}
---
+++ anxiety.js
@@ -0,0 +1,16 @@
+function calculateAnxietyScore() {
+    const scores = [];
+    for (let i = 1; i <= 7; i++) {
+        const questionId = `question${i}`;
+        const selectedValue = document.getElementById(questionId).value;
+        scores.push(parseInt(selectedValue));
+    }
+
+    const totalScore = scores.reduce((sum, score) => sum + score, 0);
+
+    if (totalScore >= 14) {
+        alert("You might be experiencing anxiety and need to start taking consultancy as soon as possible.");
+    } else {
+        alert("You are safe. If you are unsure, you can take the test again.");
+    }
+}
